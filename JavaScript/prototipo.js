9df328e63c3eb4c4b353c571139e956d6ea41b87
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-12-29 19:12:03 -0300

    Update prototipo.js

--- JavaScript/prototipo.js
+++ JavaScript/prototipo.js
@@ -1,2 +1,48 @@
-nome = prompt("Digite: ")
-console.log(nome)
+//Usando alguns tipos do JavaScript
+var idade = 18;
+var nome = "Carlos";
+var sobrenome = "Henrique";
+var verdade = true;
+
+console.log("=======================================================");
+console.log("Olá, eu me chamo " + nome + " " + sobrenome+".");
+console.log("Eu tenho " + idade +" anos.");
+console.log("Esse cara \u261D está dizendo a verdade?"+ verdade+"!");
+
+console.log("=======================================================");
+console.log("O texto acima foi feito usando caracteres UTF-16 de");
+console.log("emoji e coversões implícitas. O texto abaixo é o mesmo");
+console.log("Só que usando conversões explicitas\n\n");
+
+idade = String(idade);
+verdade = String(verdade);
+
+console.log("Olá, eu me chamo " + nome + " " + sobrenome+".");
+console.log("Eu tenho " + idade +" anos.");
+console.log("Esse cara \u261D está dizendo a verdade? "+ verdade+"!");
+
+console.log("\nSó para mostrar que eu não estou mentindo, aqui as conversões:");
+console.log("Idade: "+typeof(idade));
+console.log("verdade: "+typeof(verdade))
+
+console.log("=======================================================");
+console.log("Vamos brincar com os dias da semana, o código abaixo usa");
+console.log("classes e objetos do tipo Date que manipula os dias e horas");
+
+let hoje = new Date();
+
+console.log("Informações de hoje: " + hoje);
+console.log("Estamos no mes:"+(hoje.getMonth()+1));
+console.log("Data formatada: " + hoje.getDate() +"/"+ hoje.getMonth() +"/"+ hoje.getFullYear());
+console.log("Horário formatado: "+hoje.getHours() +":"+ hoje.getMinutes() +":"+ hoje.getSeconds() );
+
+console.log("=======================================================");
+console.log("Por fim, vamos brincar com o tipo String");
+nome = "Carlos";
+console.log("Exemplo de String: " + nome);
+console.log("Tamanho da String: "+nome.length);
+console.log("Imprimindo cada letra do meu nome:");
+
+for(x=0; x<nome.length ;x++){
+    console.log(nome.charAt(x));
+}
